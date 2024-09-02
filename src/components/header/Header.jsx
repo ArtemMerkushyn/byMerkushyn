ae6544673f7e5961617b5996f0c98605727f47d8
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-09-02 17:47:04 +0300

    wrote the navmenu for mobile display width

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import { NavLink } from 'react-router-dom';
 import styles from './header.module.scss'
+import { useState } from 'react';
 
 export const Header = () => {
+    const [ activeBurgerBtn, setActiveBurgerBtn ] = useState(false);
+
+    const toggleBurgerMenu = () => {
+        setActiveBurgerBtn(!activeBurgerBtn);
+    };
+
     return (
         <div className={styles.container}>
             <header className={styles.header}>
                 <div className={styles.logo}>{`<>Merkushyn'sSolutions</>`}</div>
-                <nav>
+                <nav className={`${styles.nav} ${activeBurgerBtn ? styles.active : ''}`}>
                     <ul className={styles.nav__items}>
                         <li className={styles.nav__item}>
                             <NavLink 
@@ -38,7 +45,47 @@ export const Header = () => {
                         </li>
                     </ul>
                 </nav>
+                <button
+                    className={`${styles.burger__btn} ${activeBurgerBtn ? styles.active : ''}`}
+                    onClick={toggleBurgerMenu}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
             </header>
+            <nav className={`${styles.navmenu} ${activeBurgerBtn ? styles.active : ''}`}>
+                <ul className={styles.navmenu__items}>
+                    <li className={styles.navmenu__item}>
+                        <NavLink 
+                            className={({ isActive }) => (isActive ? styles.active : undefined)} 
+                            to={'/'}
+                        >
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className={styles.navmenu__item}>
+                        <NavLink 
+                            className={({ isActive }) => (isActive ? styles.active : undefined)}
+                            to={'/x'}
+                        >
+                            Projects
+                        </NavLink>
+                    </li>
+                    <li className={styles.navmenu__item}>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : undefined)} 
+                            to={'/xd'}
+                        >Tech</NavLink>
+                    </li>
+                    <li className={styles.navmenu__item}>
+                        <NavLink
+                            className={({ isActive }) => (isActive ? styles.active : undefined)} 
+                            to={'/xf'}
+                        >Blog</NavLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
     );
 }
