5a133534017292b5ab2b432797c603d8b2f8c7a8
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-09-03 23:31:26 +0300

    added pictures to the hero section

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,14 +2,16 @@ import styles from './main.module.scss';
 
 export const Main = () => {
     return (
-        <div className={styles.container}>
-            <section className={styles.hero}>
+        <section className={styles.hero}>
+            <div className={styles.hero__container}>
                 <div className={styles.hero__content}>
                     <span>Hi, I'm</span>
                     <h1>Artem<br/>Merkushyn</h1>
                     <p>Full stack developer developing web applications from idea to implementation</p>
                 </div>
-            </section>
-        </div>
+            </div>
+            <img className={styles.hero__img1} src="/img/hero/hero1.png" alt="hero" />
+            <img className={styles.hero__img2} src="/img/hero/hero2.png" alt="hero" />
+        </section>
     );
 }
