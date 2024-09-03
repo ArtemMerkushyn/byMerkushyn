025593859747e033c05d18167dbeeacc7c273a7b
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-09-03 22:04:59 +0300

    wrote the hero content

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,6 +2,14 @@ import styles from './main.module.scss';
 
 export const Main = () => {
     return (
-        <div>Main</div>
+        <div className={styles.container}>
+            <section className={styles.hero}>
+                <div className={styles.hero__content}>
+                    <span>Hi, I'm</span>
+                    <h1>Artem<br/>Merkushyn</h1>
+                    <p>Full stack developer developing web applications from idea to implementation</p>
+                </div>
+            </section>
+        </div>
     );
 }
