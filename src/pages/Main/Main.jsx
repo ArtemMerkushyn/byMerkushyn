a7afd7ac1b49609304a7031f89cf7772a344652f
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-09-04 19:02:52 +0300

    worked on adaptability for iPads in the hero section

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -7,7 +7,7 @@ export const Main = () => {
                 <div className={styles.hero__content}>
                     <span>Hi, I'm</span>
                     <h1>Artem<br/>Merkushyn</h1>
-                    <p>Full stack developer developing web applications from idea to implementation</p>
+                    <p>Full stack developer, developing web applications from idea to implementation</p>
                 </div>
             </div>
             <img className={styles.hero__img1} src="/img/hero/hero1.png" alt="hero" />
