919a6b158469d65ea77fb2c2ee62dee87d663800
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-09-19 18:40:17 +0300

    added a links to my contacts in contacts components

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,12 +1,19 @@
-import Icons from "../Icons/Icons"
+import Icons from '../Icons/Icons';
+import { BsTelegram } from 'react-icons/bs';
 import styles from './contacts.module.scss';
 
 export const Contacts = () => {
   return (
     <div className={styles.contacts}>
-        <Icons icon={'whatsapp'}/>
+      <a href="https://t.me/some_guy13" target="_blank" rel="noopener noreferrer">
+        <BsTelegram/>
+      </a>
+      <a href="https://www.instagram.com/mer_kush" target="_blank" rel="noopener noreferrer">
         <Icons icon={'instagram'}/>
+      </a>
+      <a href="mailto:merkushyndev13@gmail.com" target="_blank" rel="noopener noreferrer">
         <Icons icon={'mail'}/>
+      </a>
     </div>
-  )
+  );
 }
