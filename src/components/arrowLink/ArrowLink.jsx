616a4bffb793c8740d4a5be0c11fdc55532a2745
ArtemMerkushyn <merkushyndev13@gmail.com>
2024-10-16 21:32:23 +0300

    created a simple database for projects and used it in the projects block on the main page

--- src/components/arrowLink/ArrowLink.jsx
+++ src/components/arrowLink/ArrowLink.jsx
@@ -1,8 +1,8 @@
 import { Link } from 'react-router-dom';
 import styles from './arrowLink.module.scss';
 
-export const ArrowLink = ({text}) => {
+export const ArrowLink = ({ text, url }) => {
     return (
-        <Link className={styles.link} to={'/'}>{text}</Link>
+        <Link className={styles.link} to={url}>{text}</Link>
     );
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -5,6 +5,7 @@ import Icons from '../../components/Icons/Icons.jsx';
 import { Title } from '../../components/title/Title.jsx';
 import styles from './main.module.scss';
 import { ArrowLink } from '../../components/arrowLink/ArrowLink.jsx';
+import projects from '../../db/projects.json';
 
 export const Main = () => {
     return (
@@ -30,19 +31,21 @@ export const Main = () => {
 
             <section className={styles.projects}>
                 <Title text={'Projects'} align={'center'}/>
-                <div className={styles.project}>
-                    <div className={styles.project__item}>
-                        <h5 className={styles.project__title}>Title</h5>
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem porro natus obcaecati a. Id dolorem quos pariatur vel nobis totam nulla accusamus, culpa veritatis earum deserunt aspernatur temporibus dicta, cupiditate laboriosam ex quisquam rerum, est dolore ullam. Nobis, nostrum modi soluta numquam fugit cumque veritatis maiores minus inventore nesciunt illo similique corporis sit minima officiis deserunt obcaecati pariatur animi eligendi atque dolorum. Consequatur ratione est voluptates blanditiis, iste mollitia voluptatibus nemo, voluptatem alias officia ad laboriosam? Quisquam molestiae eos magni, exercitationem aliquam repudiandae veniam, nihil sunt consequuntur dicta ipsum nam eum commodi rerum. Sunt fugiat porro ab ipsum repellat sapiente.
-                        </p>
-                        <Btn2 text={'visit app website'} url={'https://www.celoxis.com/cassets/img/pmc/project-management.png'}/>
-                        <br/>
-                        <ArrowLink text={'More info'}/>
-                    </div>
-                    <div className={styles.project__item}>
-                        <img src="https://www.celoxis.com/cassets/img/pmc/project-management.png" alt="img" />
-                    </div>
+                <div className={styles.projects__items}>
+                    {projects.map((project) => (
+                       <div key={project.id} className={styles.project}>
+                            <div className={styles.project__item}>
+                                <h5 className={styles.project__title}>{project.title}</h5>
+                                <p>{project.description}</p>
+                                <Btn2 text={'visit app website'} url={project.link}/>
+                                <br/>
+                                <ArrowLink text={'More info'} url={'https://artemmerkushyn.github.io/furni-shop/#hero'}/>
+                            </div>
+                            <div className={styles.project__item}>
+                                <img src="https://www.celoxis.com/cassets/img/pmc/project-management.png" alt="img" />
+                            </div>
+                        </div> 
+                    ))}
                 </div>
             </section>
         </>
