5b0b76233ef44b510669a15d185a13cc369980bb
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-01-07 16:59:20 +0200

    changing skills section

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,10 +2,11 @@ import { Btn1 } from '../../components/btns/btn1/Btn1.jsx';
 import { Btn2 } from '../../components/btns/btn2/Btn2.jsx';
 import { Contacts } from '../../components/contacts/Contacts.jsx';
 import Icons from '../../components/Icons/Icons.jsx';
+import { FiLayout } from "react-icons/fi";
+import { FaJsSquare, FaReact, FaNode, FaGitAlt } from "react-icons/fa";
 import { SiMongodb } from "react-icons/si";
-import { FaReact } from "react-icons/fa";
-import { FaNode } from "react-icons/fa";
-import { FaHtml5 } from "react-icons/fa";
+import { FaPeopleCarryBox } from "react-icons/fa6";
+
 import { Title } from '../../components/title/Title.jsx';
 import styles from './main.module.scss';
 import { ArrowLink } from '../../components/arrowLink/ArrowLink.jsx';
@@ -58,47 +59,64 @@ export const Main = () => {
             <div className={styles.container}>
                 <section className={styles.skills}>
                     <Title text={'Skills'} align={'center'}/>
-                    <div className={styles.skills__wrapper}>
-                        <div className={styles.skills__items}>
-                            <div className={styles.skills__img}>
-                                <img src="/img/skills/Astronaut.png" alt="astronaut" />
-                            </div>
-                            <div className={`${styles.icon} ${styles.icon1}`}><FaHtml5/></div>
-                            <div className={`${styles.icon} ${styles.icon2}`}><Icons icon="css" /></div>
-                            <div className={`${styles.icon} ${styles.icon3}`}><Icons icon="js" /></div>
-                            <div className={`${styles.icon} ${styles.icon4}`}><Icons icon="ts" /></div>
-                            <div className={`${styles.icon} ${styles.icon5}`}><SiMongodb /></div>
-                            <div className={`${styles.icon} ${styles.icon6}`}>Express</div>
-                            <div className={`${styles.icon} ${styles.icon7}`}><FaReact /></div>
-                            <div className={`${styles.icon} ${styles.icon8}`}><FaNode/></div>
+                    <div className={styles.skills__container}>
+                        <div className={styles.skills__image}>
+                            <img src="/img/skills/Astronaut.png" alt="astronaut" />
                         </div>
-                        <ul className={styles.skills__text}>
-                            <li>
-                                Знаю основы HTML, включая семантические элементы и работу с формами, что помогает создавать доступные и правильно структурированные страницы. Также имею опыт работы с CSS и SCSS для стилизации, использую Flexbox и Grid для создания адаптивных и отзывчивых интерфейсов. Работаю с SCSS для упрощения и улучшения структуры стилей, позволяя использовать переменные, миксины и наследование для более гибкой и модульной организации.
+                        <ul className={styles.skills__items}>
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FiLayout />
+                                </div>
+                                <p>
+                                    Знаю основы HTML, включая семантические элементы и работу с формами, что помогает создавать доступные и правильно структурированные страницы. Также имею опыт работы с CSS и SCSS для стилизации, использую Flexbox и Grid для создания адаптивных и отзывчивых интерфейсов. Работаю с SCSS для упрощения и улучшения структуры стилей, позволяя использовать переменные, миксины и наследование для более гибкой и модульной организации.
+                                </p>
                             </li>
-                            <br/>
-                            <li>
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FaJsSquare />
+                                </div>
                                 JavaScript у меня на базовом уровне: работаю с переменными, типами данных, функциями, циклами и операторами. Знаком с асинхронностью, манипуляцией DOM и обработкой событий. Умею работать с массивами и объектами, использовать методы массивов, а также с JSON. Разбираюсь в объектно-ориентированном программировании и основах хранения данных.
                             </li>
-                            <br/>
-                            <li>
-                                Я хорошо работаю с React, применяя функциональные компоненты и хуки. Умею настраивать маршрутизацию с React Router для создания многостраничных приложений. Использую Redux для управления состоянием и передачи данных между компонентами. Также имею опыт работы с API-запросами, включая получение и отправку данных, а также обработку асинхронных операций.
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FaReact />
+                                </div>
+                                <p>
+                                    Я хорошо работаю с React, применяя функциональные компоненты и хуки. Умею настраивать маршрутизацию с React Router для создания многостраничных приложений. Использую Redux для управления состоянием и передачи данных между компонентами. Также имею опыт работы с API-запросами, включая получение и отправку данных, а также обработку асинхронных операций.
+                                </p>
                             </li>
-                            <br/>
-                            <li>
-                                На серверной стороне использую Node.js и Express.js для разработки RESTful API, настройки маршрутов и работы с middleware. Я знаком с авторизацией через JWT и понимаю основы безопасности приложений, включая работу с CORS и настройку защищенных маршрутов.
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FaNode/>
+                                </div>
+                                <p>
+                                    На серверной стороне использую Node.js и Express.js для разработки RESTful API, настройки маршрутов и работы с middleware. Я знаком с авторизацией через JWT и понимаю основы безопасности приложений, включая работу с CORS и настройку защищенных маршрутов.
+                                </p>
                             </li>
-                            <br/>
-                            <li>
-                                Работаю с MongoDB, выполняю CRUD-операции, настраиваю индексы и использую Mongoose для удобного взаимодействия с базой данных, создавая модели и схемы. Это позволяет эффективно управлять данными и работать с большими объемами информации.
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <SiMongodb />
+                                </div>
+                                <p>
+                                    Работаю с MongoDB, выполняю CRUD-операции, настраиваю индексы и использую Mongoose для удобного взаимодействия с базой данных, создавая модели и схемы. Это позволяет эффективно управлять данными и работать с большими объемами информации.
+                                </p>
                             </li>
-                            <br/>
-                            <li>
-                                Я активно использую Git для контроля версий, работаю с ветками, слияниями и разрешением конфликтов. Это позволяет мне легко работать в команде и управлять проектами на GitHub.
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FaGitAlt />
+                                </div>
+                                <p>
+                                    Я активно использую Git для контроля версий, работаю с ветками, слияниями и разрешением конфликтов. Это позволяет мне легко работать в команде и управлять проектами на GitHub.
+                                </p>
                             </li>
-                            <br/>
-                            <li>
-                                У меня развиты навыки командной работы, коммуникации и критического мышления. Я умею решать проблемы, писать понятный код и адаптироваться к новым технологиям. Эффективно управляю временем, принимаю конструктивную критику и всегда готов помогать коллегам.
+                            <li className={styles.slills__item}>
+                                <div className={styles.skills__img}>
+                                    <FaPeopleCarryBox />
+                                </div>
+                                <p>
+                                    У меня развиты навыки командной работы, коммуникации и критического мышления. Я умею решать проблемы, писать понятный код и адаптироваться к новым технологиям. Эффективно управляю временем, принимаю конструктивную критику и всегда готов помогать коллегам.
+                                </p>
                             </li>
                         </ul>
                     </div>
