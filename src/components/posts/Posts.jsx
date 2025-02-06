decf2f30f095de82b3b1d0931d06c26d5f375f02
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-02-06 23:55:13 +0200

    fixed the posts topic block in the posts component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,7 +4,7 @@ export const Posts = () => {
     return (
         <div className={styles.posts}>
             <div className={styles.posts__topics}>
-            <button className={styles.posts__topic}>All</button>
+                <button className={styles.posts__topic}>All</button>
                 <button className={styles.posts__topic}>Programming</button>
                 <button className={styles.posts__topic}>Sport</button>
                 <button className={styles.posts__topic}>Other</button>
