e2d9dacb146a379b4c160de4b107baa00d9dba20
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-02-06 23:49:03 +0200

    wrote the styles for posts component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -4,9 +4,10 @@ export const Posts = () => {
     return (
         <div className={styles.posts}>
             <div className={styles.posts__topics}>
-                <button className={styles.posts__topic}>programming</button>
-                <button className={styles.posts__topic}>sport</button>
-                <button className={styles.posts__topic}>other</button>
+            <button className={styles.posts__topic}>All</button>
+                <button className={styles.posts__topic}>Programming</button>
+                <button className={styles.posts__topic}>Sport</button>
+                <button className={styles.posts__topic}>Other</button>
             </div>
         </div>
     );
