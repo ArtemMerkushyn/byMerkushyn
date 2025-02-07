8701f91e277195efd98c8938734dde032dcaa23f
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-02-07 23:53:18 +0200

    wrote the post block in the posts component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -9,6 +9,53 @@ export const Posts = () => {
                 <button className={styles.posts__topic}>Sport</button>
                 <button className={styles.posts__topic}>Other</button>
             </div>
+            <div className={styles.posts__items}>
+                <div className={styles.post}>
+                    <div className={styles.post__header}>Programming</div>
+                    <div className={styles.post__body}>
+                        <h4 className={styles.post__title}>The future of software engineering</h4>
+                        <p className={styles.post__text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.post}>
+                    <div className={styles.post__header}>Programming</div>
+                    <div className={styles.post__body}>
+                        <h4 className={styles.post__title}>The future of software engineering</h4>
+                        <p className={styles.post__text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.post}>
+                    <div className={styles.post__header}>Programming</div>
+                    <div className={styles.post__body}>
+                        <h4 className={styles.post__title}>The future of software engineering</h4>
+                        <p className={styles.post__text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.post}>
+                    <div className={styles.post__header}>Programming</div>
+                    <div className={styles.post__body}>
+                        <h4 className={styles.post__title}>The future of software engineering</h4>
+                        <p className={styles.post__text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.post}>
+                    <div className={styles.post__header}>Programming</div>
+                    <div className={styles.post__body}>
+                        <h4 className={styles.post__title}>The future of software engineering</h4>
+                        <p className={styles.post__text}>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
