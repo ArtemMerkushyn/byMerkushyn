3245fef5760e2a138ca4304b9057a0731debb354
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-02-08 14:49:27 +0200

    implemented post filter

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,10 +1,28 @@
 import { useState } from 'react';
 import styles from './posts.module.scss';
+import { useEffect } from 'react';
 
 export const Posts = () => {
     const [activeTopic, setActiveTopic] = useState('All');
+    const [posts, setPosts] = useState([]);
+    const [topics, setTopics] = useState(['All']);
 
-    const topics = ['All', 'Programming', 'Sport', 'Other']
+    useEffect(() => {
+        fetch('/db/posts.json') // Загружаем JSON из public
+            .then((res) => res.json())
+            .then((data) => {
+                setPosts(data);
+
+                // Получаем уникальные темы постов
+                const uniqueTopics = ['All', ...new Set(data.map(post => post.topic))];
+                setTopics(uniqueTopics);
+            })
+            .catch((err) => console.error('Error downloading posts:', err));
+    }, []);
+
+    // Фильтрация постов по выбранной теме
+    const filteredPosts = activeTopic === 'All' ? posts : posts.filter(post => post.topic === activeTopic);
+
     return (
         <div className={styles.posts}>
             <div className={styles.posts__topics}>
@@ -17,51 +35,20 @@ export const Posts = () => {
                 ))}
             </div>
             <div className={styles.posts__items}>
-                <div className={styles.post}>
-                    <div className={styles.post__header}>Programming</div>
-                    <div className={styles.post__body}>
-                        <h4 className={styles.post__title}>The future of software engineering</h4>
-                        <p className={styles.post__text}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.post}>
-                    <div className={styles.post__header}>Programming</div>
-                    <div className={styles.post__body}>
-                        <h4 className={styles.post__title}>The future of software engineering</h4>
-                        <p className={styles.post__text}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.post}>
-                    <div className={styles.post__header}>Programming</div>
-                    <div className={styles.post__body}>
-                        <h4 className={styles.post__title}>The future of software engineering</h4>
-                        <p className={styles.post__text}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.post}>
-                    <div className={styles.post__header}>Programming</div>
-                    <div className={styles.post__body}>
-                        <h4 className={styles.post__title}>The future of software engineering</h4>
-                        <p className={styles.post__text}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
-                        </p>
-                    </div>
-                </div>
-                <div className={styles.post}>
-                    <div className={styles.post__header}>Programming</div>
-                    <div className={styles.post__body}>
-                        <h4 className={styles.post__title}>The future of software engineering</h4>
-                        <p className={styles.post__text}>
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ex aperiam magnam tempore dignissimos nisi veniam at quo velit debitis sint inventore ullam illo, ad ipsum, animi quibusdam, sed natus?
-                        </p>
-                    </div>
-                </div>
+                {filteredPosts.length > 0 ? (
+                    filteredPosts.map((post) => (
+                        <div key={post._id} className={styles.post}>
+                            <div className={styles.post__header}>{post.topic}</div>
+                            <div className={styles.post__body}>
+                                <h4 className={styles.post__title}>{post.title}</h4>
+                                <p className={styles.post__text}>{post.text}</p>
+                            </div>
+                            <div className={styles.post__date}>{post.date}</div>
+                        </div>
+                    ))
+                ) : (
+                    <p className={styles.noPosts}>No posts</p>
+                )}
             </div>
         </div>
     );
