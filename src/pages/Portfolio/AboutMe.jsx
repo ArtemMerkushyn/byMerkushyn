38473c2a8f58b970ae4f286c976688acbe69c9a6
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-02-20 20:34:37 +0200

    Im still thinking about the page about me

--- src/pages/Portfolio/AboutMe.jsx
+++ src/pages/Portfolio/AboutMe.jsx
@@ -1,10 +1,16 @@
+import { Btn1 } from '../../components/btns/btn1/Btn1';
+import { Contacts } from '../../components/contacts/Contacts';
 import styles from './aboutme.module.scss';
 
 export default function AboutMe() {
   return (
     <div className={styles.container}>
         <section className={styles.aboutme}>
-            AboutMe 
+            <h3 className={styles.aboutme__title}>Artem <br/> Merkushyn</h3> 
+            <Btn1 text={'Take a look at GitHub'} url={'https://github.com/ArtemMerkushyn'}/>
+            <Contacts/>
+            <a className={styles.aboutme__call} href="tel:+380958827306">+380958827306</a>
+            <div className={styles.aboutme__lacation}>Ukraine, Kyiv</div>
         </section>
     </div>
   )
