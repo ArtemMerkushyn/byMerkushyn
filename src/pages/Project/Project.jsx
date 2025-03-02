18e503309c8035d817c2d814b450df1bf91248d4
ArtemMerkushyn <merkushyndev13@gmail.com>
2025-03-02 21:18:06 +0200

    implemented getting the project on the project page using fetch

--- src/pages/Project/Project.jsx
+++ src/pages/Project/Project.jsx
@@ -1,7 +1,26 @@
+import { useParams } from 'react-router-dom';
 import styles from './project.module.scss';
+import { useEffect, useState } from 'react';
 
 export const Project = () => {
+    const { _id } = useParams();
+    const [ project, setProject ] = useState(null);
+
+    useEffect(() => {
+        fetch('/db/projects.json')
+            .then((res) => res.json())
+            .then((data) => {
+                const foundProject = data.find((project) => project._id === _id);
+                setProject(foundProject);
+            })
+            .catch((err) => console.log('Error loading project ', err));
+    }, [_id]);
+
+    if (!project) {
+        return <div>No project</div>
+    }
+
     return (
-        <div className={styles.container}>Project</div>
+        <div className={styles.container}>{project.title}</div>
     );
 }
